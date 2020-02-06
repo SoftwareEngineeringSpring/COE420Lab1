a87a93a38814f250dffab40ddf2b2a82cc41216c
mohammed alzaabi <b00065549@aus.edu>
2020-02-06 19:08:41 +0400

    Division implemented

--- Calculations.java
+++ Calculations.java
@@ -12,7 +12,8 @@ public class Calculations {
 		//member_B add the feature of subtracting x from y and print the result
 		
 		//member_C add the feature of dividing x by y and print the result
-		
+		int quotient = x/y;
+		System.out.print("The quotient is=" + quotient);
 		//member_D add the feature of multiplying x by y and print the result
 		
 		
